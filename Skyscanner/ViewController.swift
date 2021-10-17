0f2c9c437fccc22597c83dc5468bf4e1d235c0f9
Sutie <synature14@gmail.com>
2021-10-17 21:38:48 +0900

    initialize~

---
+++ Skyscanner/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Skyscanner
+//
+//  Created by Suvely on 2021/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var name = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
